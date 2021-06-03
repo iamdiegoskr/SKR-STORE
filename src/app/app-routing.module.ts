5622606e4be962293233b48ed34c85a81eb74260
diego martinez <juandiego337@hotmail.com>
2021-06-03 00:05:04 -0500

    Add schematic forms and nav components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -27,6 +27,10 @@ const routes: Routes = [
       },
     ],
   },
+  {
+    path:'admin',
+    loadChildren:()=> import('./admin/admin.module').then(m=>m.AdminModule)
+  },
   { path: '**', component: PageNotFoundComponent }, // Wildcard route for a 404 page
 ];
 
--- src/app/components/material/material.module.ts
+++ src/app/components/material/material.module.ts
@@ -3,13 +3,16 @@ import { CommonModule } from '@angular/common';
 import { SelectOptionColorComponent } from './select/select-option-color/select-option-color.component';
 
 
-import {MatSelectModule} from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
+
 import { MatFormFieldModule } from "@angular/material/form-field";
 import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
 import {MatBadgeModule} from '@angular/material/badge';
 import { FormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -23,7 +26,9 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatRadioModule,
+    MatCardModule
   ],
   exports:[
     SelectOptionColorComponent,
@@ -31,7 +36,11 @@ import { FormsModule } from '@angular/forms';
     MatInputModule,
     MatIconModule,
     MatButtonModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatRadioModule,
+    MatCardModule,
+    FormsModule,
+    MatSelectModule
   ]
 })
 export class MaterialModule { }
