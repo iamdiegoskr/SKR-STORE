6076624daa72db4156b0a29ba4d07e569087fc7f
diego martinez <juandiego337@hotmail.com>
2021-06-08 11:40:30 -0500

    confirm dialog using sweetalert2 library

--- src/app/admin/components/list-products/list-products.component.ts
+++ src/app/admin/components/list-products/list-products.component.ts
@@ -2,49 +2,78 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import { Product } from 'src/app/interfaces/product';
 
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-list-products',
   templateUrl: './list-products.component.html',
-  styleUrls: ['./list-products.component.scss']
+  styleUrls: ['./list-products.component.scss'],
 })
 export class ListProductsComponent implements OnInit {
-
-  products:Product[] = []
+  products: Product[] = [];
   displayedColumns: string[] = ['id', 'name', 'price', 'actions'];
 
-  constructor(private service:ProductsService) { }
+  constructor(private service: ProductsService) {}
 
   ngOnInit(): void {
-        this.fecthDataProduct()
+    this.fecthDataProduct();
   }
 
-  fecthDataProduct(){
-    this.service.getAllProducts().subscribe(products=>{
-      this.products=products
+  fecthDataProduct() {
+    this.service.getAllProducts().subscribe((products) => {
+      this.products = products;
       console.log(this.products);
-    })
+    });
   }
+
+  editProduct() {}
 
   //Forma 1
-  deleteProduct(id:string){
-    this.service.deleteProduct(id).subscribe(data=>{
-      this.products = this.products.filter(element=> element.id!=id)
-    })
-  }
+  deleteProduct(id: string) {
+    console.log(id);
+    Swal.fire({
+      title: 'Estas seguro de eliminarlo?',
+      text: 'Si eliminar el producto, sera eliminado por completo',
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'Cancelar',
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.service.deleteProduct(id).subscribe((productRemoveIsOk:boolean) => {
 
-  deleteProduct2(id:string){
-    this.service.deleteProduct(id).subscribe(productRemoveIsOk=>{
-      if(productRemoveIsOk){
+          console.log(productRemoveIsOk);
 
-        const indexToRemove = this.products.findIndex(product => product.id == id)
-        this.products.splice(indexToRemove,1)
-        this.products = [...this.products]
+          if (productRemoveIsOk) {
 
-      }else{
-        console.log("Error, el producto no pudo ser elimnando.");
+            this.products = this.products.filter((element) => element.id != id);
+
+            Swal.fire(
+              'Se ha eliminado con exito',
+              'Tu producto ha sido eliminado correctamente',
+              'success'
+            );
+          }else{
+            console.log('error');
+          }
+        });
       }
-    })
+    });
   }
 
-
+  // deleteProduct2(id: string) {
+  //   this.service.deleteProduct(id).subscribe((productRemoveIsOk) => {
+  //     if (productRemoveIsOk) {
+  //       const indexToRemove = this.products.findIndex(
+  //         (product) => product.id == id
+  //       );
+  //       this.products.splice(indexToRemove, 1);
+  //       this.products = [...this.products];
+  //     } else {
+  //       console.log('Error, el producto no pudo ser elimnando.');
+  //     }
+  //   });
+  // }
 }
