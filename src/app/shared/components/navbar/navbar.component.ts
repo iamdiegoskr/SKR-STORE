f4aec19e7bd46674911728c9d49fde3bae2116d3
diego martinez <juandiego337@hotmail.com>
2021-06-10 23:15:28 -0500

    Using operatos map from rxjs to transform data

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { CartService } from 'src/app/core/services/cart/cart.service';
 
+import { map } from 'rxjs/operators'
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
@@ -12,10 +14,19 @@ export class NavbarComponent implements OnInit {
 
   constructor(private serviceCart:CartService) {
 
-    this.serviceCart.cart$.subscribe(products=>{
+    // this.serviceCart.cart$.subscribe(products=>{
+    //   console.log("Desde el navbar");
+    //   console.log(products);
+    //   this.quantity = products.length;
+    // })
+
+    this.serviceCart.cart$
+    .pipe(
+      map(products=>products.length)
+    )
+    .subscribe(productsQuantity=>{
       console.log("Desde el navbar");
-      console.log(products);
-      this.quantity = products.length;
+      this.quantity = productsQuantity;
     })
 
   }
